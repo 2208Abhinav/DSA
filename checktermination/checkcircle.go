a99abf88b4528c4199e9fd6c0720f46fc819f2d9
Abhinav Singh <abhinavsingh22@hotmail.com>
2019-03-16 16:26:42 +0530

    Added my algo for the following question.
    My algo was faster than 100% other solutions made in golang.
    
    You are given two non-empty linked lists representing two
    non-negative integers. The digits are stored in reverse order
    and each of their nodes contain a single digit.
    Add the two numbers and return it as a linked list.
    
    You may assume the two numbers do not contain any leading zero,
    except the number 0 itself.
    
    Example:
    
    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
    Output: 7 -> 0 -> 8
    Explanation: 342 + 465 = 807.

--- checktermination/checkcircle.go
+++ checktermination/checkcircle.go
@@ -56,28 +56,58 @@ func checkTermination(current *Node) string {
 	return "Not trapped in circular list!"
 }
 
+func addTwoNumbers(l1 *Node, l2 *Node) *Node {
+	c1 := l1
+	c2 := l2
+	sum := 0
+	nextSum := 0
+
+	head := &Node{}
+	var currentHead *Node
+	for true {
+		if c1 != nil || c2 != nil {
+			if c1 == nil {
+				sum = nextSum + c2.value
+				nextSum = sum / 10
+				c2 = c2.next
+			} else if c2 == nil {
+				sum = nextSum + c1.value
+				nextSum = sum / 10
+				c1 = c1.next
+			} else {
+				sum = nextSum + c1.value + c2.value
+				nextSum = sum / 10
+				c1 = c1.next
+				c2 = c2.next
+			}
+			fmt.Println(sum % 10)
+
+			if head.value == 0 {
+				head.value = sum % 10
+				head.next = &Node{}
+				currentHead = head
+			} else {
+				currentHead.next = &Node{sum % 10, nil}
+				currentHead = currentHead.next
+			}
+			if nextSum != 0 {
+				// If nextSum is not 0 this means that there was some carry value
+				// left in it which should be further.
+				currentHead.next = &Node{nextSum, nil}
+			}
+		} else {
+			break
+		}
+	}
+
+	return head
+}
+
 func main() {
-	n1 := &Node{1, nil}
-	n2 := &Node{2, nil}
-	n3 := &Node{3, nil}
-	n4 := &Node{4, nil}
-	n5 := &Node{5, nil}
-	n6 := &Node{6, nil}
-	n7 := &Node{7, nil}
-	n8 := &Node{8, nil}
-	n9 := &Node{9, nil}
-	n10 := &Node{10, nil}
+	l1 := &Node{9, &Node{8, nil}}
+	l2 := &Node{9, &Node{3, nil}}
 
-	n1.next = n2
-	n2.next = n3
-	n3.next = n4
-	n4.next = n5
-	n5.next = n6
-	n6.next = n7
-	n7.next = n8
-	n8.next = n9
-	n9.next = n10
-	n10.next = n4
-
-	fmt.Println(flyodDetection(n1))
+	// fmt.Println(flyodDetection(n1))
+	// n1.insertInSorted(-1212)
+	printList(addTwoNumbers(l1, l2))
 }
