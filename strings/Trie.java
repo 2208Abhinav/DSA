4680aa9cbd94e976b0e0463f0fefafe589732589
Abhinav Singh <abhinavsingh22@hotmail.com>
2019-08-24 14:03:08 +0530

    Implement Trie which gives O(m) search time complexity. m is length of the key being searched.

---
+++ strings/Trie.java
@@ -0,0 +1,69 @@
+class TrieNode {
+	boolean isEndOfWord;
+	TrieNode[] children = new TrieNode[26];
+
+	TrieNode() {
+		this.isEndOfWord = false;
+	}
+}
+
+public class Trie {
+	static TrieNode root;
+
+	// Worst case time complexity of insert: O(m)
+	// m is the length of key
+	static void insert(String key) {
+		TrieNode nextNode = root;
+		int length = key.length();
+
+		for(int i = 0; i < length; i++) {
+			int index = key.charAt(i) - 'a';
+
+			if (nextNode.children[index] == null) {
+				nextNode.children[index] = new TrieNode();
+			}
+			nextNode = nextNode.children[index];
+		}
+
+		nextNode.isEndOfWord = true;
+	}
+
+	// Worst case time complexity of search: O(m)
+	// m is the length of key
+	static boolean search(String key) {
+		TrieNode nextNode = root;
+		int length = key.length();
+
+		for(int i = 0; i < length; i++) {
+			int index = key.charAt(i) - 'a';
+
+			if (nextNode.children[index] == null) return false;
+
+			nextNode = nextNode.children[index];
+		}
+
+		return nextNode.isEndOfWord;
+	}
+
+	public static void main(String[] args) {
+		root = new TrieNode();
+
+		// This Trie supports small letters containing a, b,..., z
+		String[] keysList = new String[]{"asa", "asasas", "avda", "dvc", "zasas", "casas"};
+
+		int keysListLen = keysList.length;
+
+		for(int i = 0; i < keysListLen; i++) {
+			insert(keysList[i]);
+		}
+
+		String[] searchKeys = new String[]{"bcd", "asa", "dvc", "zas", "zasas", "a"};
+
+		int searchKeysListLen = searchKeys.length;
+
+		for(int i = 0; i < searchKeysListLen; i++) {
+			boolean isPresent = search(searchKeys[i]);
+			System.out.println(searchKeys[i] + " exists: " + isPresent);
+		}
+	}
+}
